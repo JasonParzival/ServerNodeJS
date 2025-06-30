0913476e7ee840e06ec9a9924f8966407b2f97c9
JasonParzival <ruslanmuhamed123@gmail.com>
2025-06-30 19:03:17 +0800

    Я удивлён, при инициилизации проекта, в package.json записались ссылки на мой проект в гите

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const http = require("http");
 http.createServer(function(request,response){
      
-    response.end("Hello NodeJS!");
+    response.writeHead(200, {
+        "Content-Type": "text/html; charset=utf-8"
+    });
+
+    response.end("<h1>Привет, Октагон!</h1>");
      
 }).listen(3000, "127.0.0.1",function(){
     console.log("Сервер начал прослушивание запросов на порту 3000");
